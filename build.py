1a31e788546fe0a252ba037f6e127688a5a9ebda
ajskateboarder <81255936+themysticsavages@users.noreply.github.com>
2021-09-03 08:51:48 -0700

    zip support

--- build.py
+++ build.py
@@ -1,12 +1,13 @@
 from src.exceptions import InvalidInt
 from src.analyze import *
-import src.html
+import src.pack
 import argparse
 
 parser = argparse.ArgumentParser(description='Throw together all the assets of a Scratch projects as local files.')
 
 parser.add_argument('--html', action='store', help='Create an HTML file showing all the assets in one place. Creates in local directory.')
 parser.add_argument('--id', help='Project ID for fetching assets. Totally required!', required=True)
+parser.add_argument('--zip', action='store_true', help='Put all the assets in a zip file.')
 
 args = parser.parse_args()
 
@@ -17,6 +18,8 @@ else:
     if args.html:
         try:
             args.html.split('//')[1] == 'popex'
-            src.html.use(res).create(str(args.html).split('//')[0], popex=True)
+            src.pack.use(res).create(str(args.html).split('//')[0], popex=True)
         except IndexError:    
-            src.html.use(res).create(str(args.html))
+            src.pack.use(res).create(str(args.html))
+    if args.zip:
+        src.pack.zip()
